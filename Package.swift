efd8e69f6dc25091db6df6d68d04af0148b8d15f
Adam Mork <adammork@gmail.com>
2023-04-24 21:34:24 -0700

    fix import paths again

--- Package.swift
+++ Package.swift
@@ -38,13 +38,13 @@ let package = Package(
         .target(
             name: "Core",
             dependencies: [.target(name: "Common"), .product(name: "GRPC", package: "grpc-swift")],
-            path: "Sources/Generated/Proto/GRPC"
+            path: "Sources/GRPC"
         ),
         .binaryTarget(
             name: "LibMobileCoin",
             url: "https://yus.s3.us-east-1.amazonaws.com/bundle.zip",
             // url: "https://github.com/mobilecoinofficial/libmobilecoin/blob/adam/%23184377543-3/Artifacts/bundle.zip",
-            checksum: "8228b2ae1c07d092e8714ebf5d064ca61303726a79c971cff5980c84f27bc344")
+            checksum: "33d4a839ab2c3ca87754ac4c5a6a658a77429b58202254152c5923629718fc36")
     ]
 )
 
