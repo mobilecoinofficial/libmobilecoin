3743a928525c58ed3c92477c602bbb2798f2acf1
mobilecoin-ci <mobilecoin-ci@mobilecoin.com>
2023-06-02 23:00:42 +0000

    [skip ci] commit build Artifacts and generated protos from build machine

--- Sources/Generated/Proto/view.http.swift
+++ Sources/Generated/Proto/view.http.swift
@@ -20,7 +20,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+#if canImport(SwiftProtobuf)
 import SwiftProtobuf
+#endif
 
 
 /// Usage: instantiate `FogView_FogViewAPIRestClient`, then call methods of this protocol to make API calls.
